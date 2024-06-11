9f43fe3e989aef8e76fd0f81a17196ba2cca5c68
CecNyff <cecile.nyffeler@wsl.ch>
2024-06-11 13:57:53 +0200

    R unzip file function

--- src/utils.R
+++ src/utils.R
@@ -18,3 +18,23 @@ download_file <- function(url, destination_file){
 # Usage :
 # download_file("https://data.geo.admin.ch/ch.swisstopo.swissboundaries3d/swissboundaries3d_2024-01/swissboundaries3d_2024-01_2056_5728.shp.zip",
 #              "../../data/swissBOUNDARIES3D.zip")
+
+# Function to unzip file
+unzip_one_file <- function(zipfile,filename, destination_file){
+  # make sure the directory exists
+  if(!file.exists(zipfile)){
+    print("File to unzip doesn't exist!")
+  } else {
+    unzip(zipfile= zipfile, files=filename, exdir = destination_file)
+  }
+}
+
+# Usage:
+# unzip_one_file("../../data/swissBOUNDARIES3D.zip", NULL, "../../data/swissBOUNDARIES3D/")
+# unzip_one_file("../../data/swissBOUNDARIES3D.zip", "swissBOUNDARIES3D_1_5_TLM_BEZIRKSGEBIET.cpg", "../../data/swissBOUNDARIES3D2/")
+
+
+
+
+
+
