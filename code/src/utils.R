904769f0beef405ef831fe6a063602a9d17ab606
alBrnd <aline.bornand@wsl.ch>
2024-06-11 13:36:31 +0200

    new folder structure

--- code/src/utils.R
+++
@@ -1,5 +0,0 @@
-# Function to get the git hash
-gitHash <- function(basename, ext){
-  fullHash <- system("git rev-parse HEAD", intern=TRUE)
-  paste0(basename,'-',substring(fullHash,1, 10), '.',ext)
-}
