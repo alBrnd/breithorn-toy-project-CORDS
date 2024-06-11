fa60a76b50c20f9f0a0787a79652d3572b3a5bb9
alBrnd <aline.bornand@wsl.ch>
2024-06-11 13:30:44 +0200

    update folder structure

--- code/src/utils.py
+++ code/src/utils.py
@@ -8,6 +8,8 @@ Created on Tue Jun 11 09:13:22 2024
 # adding git-hash to a filename
 
 import git
+import os
+import requests
 
 def generate_versioned_filename(basename, ext):
     """
@@ -36,5 +38,60 @@ def generate_versioned_filename(basename, ext):
     return filename
 
 
+
+
+def download_file(url, destination_file):
+    """
+    # Example usage:
+    url = "https://example.com/datafile.txt"
+    destination_file = "data/datafile.txt"
+    download_file(url, destination_file)
+    """
+
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(destination_file), exist_ok=True)
+
+    if os.path.isfile(destination_file):
+        # Do nothing if the file already exists
+        print(f"Already downloaded {destination_file}")
+    else:
+        # Download the file
+        print(f"Downloading {destination_file} ... ", end="")
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+
+        with open(destination_file, 'wb') as f:
+            f.write(response.content)
+        print("done.")
+
+
+
+import zipfile
+
+def unzip_one_file(zipfile_path, filename, destination_file):
+    """
+    Unzips a specific file from a zip archive and saves it to a specified location.
+
+    Parameters:
+    zipfile_path (str): Path to the zip file.
+    filename (str): The name of the file to extract from the zip file.
+    destination_file (str): The destination path where the extracted file will be saved.
+    """
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(destination_file), exist_ok=True)
+
+    with zipfile.ZipFile(zipfile_path, 'r') as z:
+        if filename in z.namelist():
+            with z.open(filename) as source, open(destination_file, 'wb') as target:
+                target.write(source.read())
+            print(f"Extracted {filename} to {destination_file}")
+        else:
+            print(f"{filename} not found in the zip archive.")
+
+# Example usage:
+# zipfile_path = 'data/archive.zip'
+# filename = 'datafile.txt'
+# destination_file = 'data/unzipped_datafile.txt'
+# unzip_one_file(zipfile_path, filename, destination_file)
 
     
