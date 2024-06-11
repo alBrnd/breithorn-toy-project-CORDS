ad6388fb1d147300f5213a93a43b0e0e40f7b74a
CecNyff <cecile.nyffeler@wsl.ch>
2024-06-11 13:41:58 +0200

    Added R function to download file

--- src/utils.R
+++ src/utils.R
@@ -3,3 +3,18 @@ gitHash <- function(basename, ext){
   fullHash <- system("git rev-parse HEAD", intern=TRUE)
   paste0(basename,'-',substring(fullHash,1, 10), '.',ext)
 }
+
+# Function to downlaod a file
+download_file <- function(url, destination_file){
+  if(file.exists(destination_file)){
+    print(paste0("Already downloaded ", destination_file))
+  } else {
+    print(paste0("Downloading ", destination_file, " ..."))
+    download.file(url=url, destfile = destination_file)
+    print("Done!")
+  }
+}
+
+# Usage :
+# download_file("https://data.geo.admin.ch/ch.swisstopo.swissboundaries3d/swissboundaries3d_2024-01/swissboundaries3d_2024-01_2056_5728.shp.zip",
+#              "../../data/swissBOUNDARIES3D.zip")
